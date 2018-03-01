723aca81f45488bd0b0336a7fc4dc3cc4f74933e
Deric Khoo <deric.kjw@gmail.com>
2018-03-01 12:47:35 +0800

    add ;
    add ;

--- createTables.sql
+++ createTables.sql
@@ -1,6 +1,3 @@
-
-
-
 create table users (
 	UID varchar(10),
 	userName varchar(50),
@@ -8,7 +5,7 @@ create table users (
 	dateJoined date,
 	isAdmin boolean,
     PRIMARY KEY (userName)
-    )
+    );
 
 create table projectsOwnership (
 	projectName varchar(100) NOT NULL,
@@ -21,7 +18,7 @@ create table projectsOwnership (
 	progress int NOT NULL CHECK (progress >= 0 and progress <=1),
 	category varchar(20),
 	PRIMARY KEY (projectID, ownerName),
-	FOREIGN KEY (ownerName) REFERENCES users(userName))
+	FOREIGN KEY (ownerName) REFERENCES users(userName));
 
 create table investments (
 	amount int NOT NULL CHECK (amount > 0),
@@ -32,7 +29,7 @@ create table investments (
 	ownerName varchar(50) NOT NULL,
 	PRIMARY KEY (investmentID),
 	FOREIGN KEY (projectID, ownerName) REFERENCES projectOwnership(projectID, ownerName),
-	FOREIGN KEY (investorName) REFERENCES users(userName))
+	FOREIGN KEY (investorName) REFERENCES users(userName));
 
 
 INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin)
@@ -52,7 +49,7 @@ values ('00005', 'Elton', '12345', date '2018-02-03', true);
 
 
 INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category)
-values ('Seeking funding for startup', 'new start up involved in property tech', date '2018-02-10', date '2018-03-10', '00001', 'Bob', 10000, 0, 'Start Ups Funding')
+values ('Seeking funding for startup', 'new start up involved in property tech', date '2018-02-10', date '2018-03-10', '00001', 'Bob', 10000, 0, 'Start Ups Funding');
 
 INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category)
 values ('Seeking funding for mobile app', 'mobile app called instagram', date '2018-02-15', date '2018-06-15', '00001', 'Alice', 50000, 0, 'Mobile App Funding');
@@ -61,7 +58,7 @@ INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDat
 values ('Seeking funding for building new hospital', 'TTS hospital', date '2018-02-20', date '2018-07-20', '00002', 'Bob', 1000000, 0, 'Others');
 
 INSERT INTO investments(armount, dateInvested, investorName, projectID, ownerName)
-values (500, date '2018-02-23', 'David', '00001', 'Bob')
+values (500, date '2018-02-23', 'David', '00001', 'Bob');
 
 INSERT INTO investments(armount, dateInvested, investorName, projectID, ownerName)
 values (500, date '2018-02-23', 'Bob', '00001', 'Alice')
