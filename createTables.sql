945b2df4f6b91ad901ecec39e79d9a608853d269
fongwz <schifx95@gmail.com>
2018-03-01 18:50:06 +0800

    Add files via upload

--- createTables.sql
+++ createTables.sql
@@ -33,23 +33,23 @@ create table investments (
 
 
 INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin)
-values ('1', 'Bob', '12345', date '2018-02-01', false);	
+values ('1', 'Bob', '12345', date '2018-02-01', false);
 
 INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin)
-values ('2', 'Alice', '12345', date '2018-02-02', false);	
+values ('2', 'Alice', '12345', date '2018-02-02', false);
 
 INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin)
-values ('3', 'Charles', '12345', date '2018-02-02', false);	
+values ('3', 'Charles', '12345', date '2018-02-02', false);
 
 INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin)
-values ('4', 'David', '12345', date '2018-02-03', true);	
+values ('4', 'David', '12345', date '2018-02-03', true);
 
 INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin)
-values ('5', 'Elton', '12345', date '2018-02-03', true);	
+values ('5', 'Elton', '12345', date '2018-02-03', true);
 
 
 INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category)
-
+values ('Seeking funding for startup', 'new start up involved in property technology', date '2018-02-10', date '2018-03-10', 1, 'Bob', 10000, 0, 'Start Ups funding');
 
 INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category)
 values ('Seeking funding for mobile app', 'mobile app called instagram', date '2018-02-15', date '2018-06-15', '1', 'Alice', 50000, 0, 'Mobile App Funding');
@@ -60,4 +60,7 @@ values ('Seeking funding for building new hospital', 'TTS hospital', date '2018-
 
 INSERT INTO investments(amount, dateInvested, investmentid, investorName, projectID, ownerName)
 values (500, date '2018-02-23', '2', 'Bob', '1', 'Alice');
+
+INSERT INTO investments(amount, dateInvested, investmentid, investorName, projectID, ownerName)
+values (500, date '2018-02-23', '1', 'David', '1', 'Alice');
 	
