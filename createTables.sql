3bca7f7444241343f4ac9e59deaaa6b80a877814
zhiliang <chenzhiliang94@gmail.com>
2018-04-11 22:46:16 +0800

    Add fake data for schema

--- createTables.sql
+++ createTables.sql
@@ -52,20 +52,93 @@ INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billing
 values ('3', 'Charles', '12345678', date '2018-02-02', false, true, 'Blk 3, Yio Chu Kang St 2, 1-20');
 
 INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
-values ('4', 'David', '12345678', date '2018-02-03', true, false, 'Blk 4, Toa Payoh St 61, 12-23');
+values ('4', 'David', '12345678', date '2018-02-03', false, false, 'Blk 4, Toa Payoh St 61, 12-23');
 
 INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
-values ('5', 'Elton', '12345678', date '2018-02-03', true, false, 'Blk 5, Lim Chu Kang, 5-02');
+values ('6', 'Fred', '12345678', date '2018-02-03', false, false, 'Blk 15, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('7', 'Greg', '12345678', date '2018-02-03', false, false, 'Blk 25, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('8', 'Hillary', '12345678', date '2018-02-03', false, false, 'Blk 35, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('9', 'Ian', '12345678', date '2018-02-03', false, false, 'Blk 45, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('10', 'John', '12345678', date '2018-02-03', false, false, 'Blk 55, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('11', 'Konghee', '12345678', date '2018-02-03', false, false, 'Blk 65, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('12', 'lauren', '12345678', date '2018-02-03', false, false, 'Blk 75, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('13', 'max', '12345678', date '2018-02-03', false, false, 'Blk 85, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('14', 'Noel', '12345678', date '2018-02-03', false, false, 'Blk 95, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('15', 'Opasd', '12345678', date '2018-02-03', false, false, 'Blk 15, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('16', 'Prince', '12345678', date '2018-02-03', false, false, 'Blk 25, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('17', 'Queen', '12345678', date '2018-02-03', false, false, 'Blk 35, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('18', 'Rabak', '12345678', date '2018-02-03', false, false, 'Blk 45, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('19', 'Steven', '12345678', date '2018-02-03', false, false, 'Blk 55, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('20', 'Ted', '12345678', date '2018-02-03', false, false, 'Blk 65, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('21', 'Umbridge', '12345678', date '2018-02-03', false, false, 'Blk 75, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('22', 'Violin', '12345678', date '2018-02-03', false, false, 'Blk 85, Lim Chu Kang, 5-02');
+
+INSERT INTO users(UID, userName, pssword, dateJoined, isAdmin, isBanned, billingAddress)
+values ('23', 'wilson', '12345678', date '2018-02-03', false, false, 'Blk 95, Lim Chu Kang, 5-02');
+
 
 
 INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
 values ('Seeking funding for startup', 'new start up involved in property technology', date '2018-02-10', date '2018-03-10', 1, 'Bob', 10000, 0, 'Start Ups funding', 'ACTIVE');
 
 INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
-values ('Seeking funding for mobile app', 'mobile app called instagram', date '2018-02-15', date '2018-06-15', '1', 'Alice', 50000, 0, 'Mobile App Funding', 'ACTIVE');
+values ('Seeking funding for mobile app', 'mobile app called instagram', date '2018-02-15', date '2018-06-15', '1', 'Alice', 50000, 1000, 'Mobile App Funding', 'ACTIVE');
 
 INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
 values ('Seeking funding for building new hospital', 'TTS hospital', date '2018-02-20', date '2018-07-20', '2', 'Bob', 1000000, 0, 'Others', 'ACTIVE');
+
+INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
+values ('Building a new software app', 'Need some funding to rent a server', date '2018-02-20', date '2018-07-20', '1', 'Ted', 1000, 0, 'Mobile App Funding', 'ACTIVE');
+
+INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
+values ('New robot funding', 'I need funds to build a robot', date '2018-02-21', date '2018-07-20', '2', 'Ted', 1000000, 0, 'Others', 'ACTIVE');
+
+INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
+values ('New game idea', 'Need funding for this new game called AAA', date '2018-04-02', date '2018-10-20', '2', 'Alice', 1000000, 0, 'Mobile App Funding', 'ACTIVE');
+
+INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
+values ('Fund my world tour please', 'I hope to travel the world by walking', date '2018-03-25', date '2018-07-20', '1', 'David', 5000, 0, 'Others', 'ACTIVE');
+
+INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
+values ('Creating a band', 'Need some funding for band instruments', date '2018-01-24', date '2018-07-20', '2', 'David', 1000, 0, 'Others', 'ACTIVE');
+
+INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
+values ('Food delivery startup', 'fund my food delivery start up', date '2018-02-23', date '2018-07-20', '3', 'David', 1000000, 0, 'Others', 'ACTIVE');
+
+INSERT INTO projectsOwnership(projectName, projectDescription, startDate, endDate, projectID, ownerName, targetAmount, progress, category, projectStatus)
+values ('handicraft project funding', 'Appreciate it if you could fund this small project', date '2018-02-20', date '2018-05-20', '3', 'Alice', 200, 0, 'Others', 'ACTIVE');
 
 
 INSERT INTO investments(amount, dateInvested, investmentid, investorName, investmentType, projectID, ownerName)
